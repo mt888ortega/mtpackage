61322ad2b24f08a4abc08a9934cccffe3b596ecf
Mateo Ortega <mmmmmateo8888@gmail.com>
2022-11-29 08:36:40 -0500

    ELIMINANDO ARCHICO

--- archivo2.java
+++
@@ -1,9 +0,0 @@
-public class archivo2 {
-    public static void main(String[] args) {
-        System.out.println("MACACO FELIZ");
-        System.out.println("MACACO TETRIS");
-        
-    }
-    
-}
-
