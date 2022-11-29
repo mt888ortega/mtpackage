8442620b7924144b2356dd0d2cf4e2777bd7207d
Mateo Ortega <mmmmmateo8888@gmail.com>
2022-11-29 09:50:26 -0500

    actualixando archivo

--- archivo2.java
+++ archivo2.java
@@ -1,3 +1,11 @@
 public class archivo2 {
+    public static void main(String[] args) {
+        
     
+    System.out.println("wasaaaa");
+    System.out.println("mateoooo");
+    
+    System.out.println("");
+    
+    }
 }
